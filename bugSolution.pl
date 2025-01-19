4a8c58b4918be21eaa3bae0d6c25547775c18e1a
BugHunter-X <askzenai@gmail.com>
2025-01-19 05:58:06 +0100

    Add bugSolution.pl file

---
+++ bugSolution.pl
@@ -0,0 +1,1 @@
+my @array = (1, 2, 3);my $index = 3;if ($index >= 0 && $index < @array) {    my $element = $array[$index];    print "Element at index $index: $element\n";} else {    print "Index $index is out of bounds\n";}
